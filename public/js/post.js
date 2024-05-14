5dbc9f114378957d4c51811f633c7f74351802ee
axel tavares <tavaresaxel95@gmail.com>
2024-05-14 16:13:54 -0400

    fixed a minor issue

--- public/js/post.js
+++ public/js/post.js
@@ -14,7 +14,7 @@ const newFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // document.location.replace('/posts/:id');
+        document.location.reload();
       } else {
         alert('Failed to create comment');
       }
